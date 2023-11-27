328cb37d87777754d5db4c88aad9cbacebf627c5
amanbhagat41 <34489796+amanbhagat41@users.noreply.github.com>
2023-11-27 18:36:40 -0500

    comments

--- js/script.js
+++ js/script.js
@@ -4,26 +4,26 @@ GUI Assignment: HW4 Using the jQuery Plugin/UI with Your Dynamic Table
 Aman Bhagat, Umass Lowell Computer Science, aman_bhagat@student.uml.edu
 Copyright (c) 2021 by Aman.  All rights reserved.  May be freely copied or 
 excerpted for educational purposes with credit to the author. 
-updated by AB on Nov 6 4:30 pm
+updated by AB on Nov 13 4:30 pm
 */
 
 $(function () {
   // makeTable()
-  $("#minColSlider").slider({
+  $("#minColSlider").slider({ //creates a slider for mincol
     min: -50,
     max: 50,
     slide: function (event, ui) {
       $("#minCol").val(ui.value);
-      $("form[name='mTable']").first().trigger("submit");
+      $("form[name='mTable']").first().trigger("submit"); //Submits the table
     },
   });
-  $("#minCol").on("keyup", function () { //https://codepen.io/DriftwoodJP/pen/zVJjrm https://codepen.io/anon/pen/bExger
+  $("#minCol").on("keyup", function () { // used https://codepen.io/DriftwoodJP/pen/zVJjrm https://codepen.io/anon/pen/bExger to make the textbox change the sliders
     $("#minColSlider").slider("value", this.value);
     if ($("form[name='mTable']").valid() == true) { //https://jqueryvalidation.org/valid/
-      $("form[name='mTable']").first().trigger("submit");
+      $("form[name='mTable']").first().trigger("submit");//Submits the table
     }
   });
-  $("#maxColSlider").slider({
+  $("#maxColSlider").slider({//creates a slider for maxCol
     min: -50,
     max: 50,
     slide: function (event, ui) {
@@ -31,13 +31,13 @@ $(function () {
       $("form[name='mTable']").first().trigger("submit");
     },
   });
-  $("#maxCol").on("keyup", function () {
+  $("#maxCol").on("keyup", function () {//make the textbox change the sliders
     $("#maxColSlider").slider("value", this.value);
     if ($("form[name='mTable']").valid() == true) {
       $("form[name='mTable']").first().trigger("submit");
     }
   });
-  $("#minRowSlider").slider({
+  $("#minRowSlider").slider({//creates a slider for minRow
     min: -50,
     max: 50,
     slide: function (event, ui) {
@@ -45,13 +45,13 @@ $(function () {
       $("form[name='mTable']").first().trigger("submit");
     },
   });
-  $("#minRow").on("keyup", function () {
+  $("#minRow").on("keyup", function () {//make the textbox change the sliders
     $("#minRowSlider").slider("value", this.value);
     if ($("form[name='mTable']").valid() == true) {
       $("form[name='mTable']").first().trigger("submit");
     }
   });
-  $("#maxRowSlider").slider({
+  $("#maxRowSlider").slider({//creates a slider for maxRow
     min: -50,
     max: 50,
     slide: function (event, ui) {
@@ -59,7 +59,7 @@ $(function () {
       $("form[name='mTable']").first().trigger("submit");
     },
   });
-  $("#maxRow").on("keyup", function () {
+  $("#maxRow").on("keyup", function () {//make the textbox change the sliders
     $("#maxRowSlider").slider("value", this.value);
     if ($("form[name='mTable']").valid() == true) {
       $("form[name='mTable']").first().trigger("submit");
@@ -67,38 +67,38 @@ $(function () {
   });
   $(document).ready(function () {
     //https://stackoverflow.com/questions/32587177/jquery-validate-compare-two-fields
-    jQuery.validator.addMethod("comparison", function (value, element, param) {
+    jQuery.validator.addMethod("comparison", function (value, element, param) {//jquery Valadation for comparing two values
       return (
         this.optional(element) || parseInt(value) >= parseInt($(param).val())
       );
     });
   });
-  $.validator.addMethod("noDecimal", function (value, element) {
+  $.validator.addMethod("noDecimal", function (value, element) { //jquery valadation for having no decimals
     return !(value % 1);
   });
 
-  $("form[name='mTable']").validate({
+  $("form[name='mTable']").validate({ //validate the form
     rules: {
-      minCol: {
+      minCol: { //rules for minCol
         required: true,
         number: true,
         noDecimal: true,
         range: [-50, 50],
       },
-      maxCol: {
+      maxCol: {//rules for maxCol
         required: true,
         number: true,
         comparison: "#minCol",
         noDecimal: true,
         range: [-50, 50],
       },
-      minRow: {
+      minRow: {//rules for minRow
         required: true,
         number: true,
         noDecimal: true,
         range: [-50, 50],
       },
-      maxRow: {
+      maxRow: {//rules for maxRow
         required: true,
         number: true,
         comparison: "#minRow",
@@ -106,7 +106,7 @@ $(function () {
         range: [-50, 50],
       },
     },
-    messages: {
+    messages: {//Messages for when the rule is violated
       minCol: {
         required: "<p id = eror>Please enter your minCol</p>",
         number: "<p id = eror>Please enter a Number</p>",
@@ -134,7 +134,7 @@ $(function () {
         noDecimal: "<p id = eror>No Decimals</p>",
       },
     },
-    submitHandler: function (form, e) {
+    submitHandler: function (form, e) { //after valadations submit
       e.preventDefault();
       makeTable();
     },
@@ -147,23 +147,23 @@ $(function () { //https://codepen.io/bobo52310/pen/ZEwBar https://codepen.io/lal
     console.log("Hi");
     $("#tabs").tabs();
     console.log(numTab);
-    $("div#tabs ul").append('<li class="tab"><input type="checkbox"><a href="#tab-' + numTab +'">Table ' +numTab + '</a><span class="ui-icon ui-icon-close""></li>');
-    $("div#tabs").append('<div id="tab-' + numTab + '">' + $("#table").html() + "</div>"); // https://stackoverflow.com/questions/34761583/how-to-clone-the-content-of-a-tab-to-the-rest-of-the-tabs-jquery
+    $("div#tabs ul").append('<li class="tab"><input type="checkbox"><a href="#tab-' + numTab +'">Table ' +numTab + '</a><span class="ui-icon ui-icon-close""></li>'); //creates a tab
+    $("div#tabs").append('<div id="tab-' + numTab + '">' + $("#table").html() + "</div>"); // https://stackoverflow.com/questions/34761583/how-to-clone-the-content-of-a-tab-to-the-rest-of-the-tabs-jquery puts table into tab
     $("div#tabs").tabs("refresh");
-    $("#tabs").tabs("option", "active", -1);
+    $("#tabs").tabs("option", "active", -1); //goes into tab
     numTab++;
   });
 });
 
 var tabs = $("#tabs").tabs();
 
-tabs.delegate("span.ui-icon-close", "click", function () {
+tabs.delegate("span.ui-icon-close", "click", function () {//deletes a single tab
   var panelId = $(this).closest("li").remove().attr("aria-controls");
   $("#" + panelId).remove();
   tabs.tabs("refresh");
   console.log(panelId);
 });
-$("#delTabs").on("click", function () { //https://stackoverflow.com/questions/26730986/delete-checked-checkboxes-in-jquery
+$("#delTabs").on("click", function () { //https://stackoverflow.com/questions/26730986/delete-checked-checkboxes-in-jquery deletes all selected tabs
   $("input:checkbox").each(function () {
     if ($(this).is(":checked")) {
       var tabId = $(this).parent().remove().attr("aria-controls");
@@ -173,7 +173,7 @@ $("#delTabs").on("click", function () { //https://stackoverflow.com/questions/26
   });
 });
 
-function makeTable() {
+function makeTable() { //makes the table
   var minCol = Number(document.getElementById("minCol").value);
   var maxCol = Number(document.getElementById("maxCol").value);
   var minRow = Number(document.getElementById("minRow").value);
